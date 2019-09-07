b2c12b2cfddf298b1349eda9c798576050e71b7b
lukecrum20 <lukecrum20@gmail.com>
2019-09-07 01:10:53 -0400

    settings menu

---
+++ src/js/controllers/index_controller.js
@@ -0,0 +1,6 @@
+function expand_settings() {
+  document.getElementById("settings").style.width = "250px";
+}
+function collapse_settings() {
+  document.getElementById("settings").style.width = "0px";
+}

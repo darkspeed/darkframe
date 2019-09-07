7b00645d1f325df6c98718f953864c67d39eb5a2
lukecrum20 <lukecrum20@gmail.com>
2019-09-07 00:04:22 -0400

    start styling

--- src/js/main.js
+++ src/js/main.js
@@ -9,13 +9,14 @@ function createWindow () {
   win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: "hidden",
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('src/views/index.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
@@ -36,20 +37,15 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+
   if (win === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.

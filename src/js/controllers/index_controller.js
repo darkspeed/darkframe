d32cc90917fa9ea07508616392beccf482d0d172
lukecrum20 <lukecrum20@gmail.com>
2019-09-09 10:05:48 -0400

    work on settings menu

--- src/js/controllers/index_controller.js
+++ src/js/controllers/index_controller.js
@@ -1,6 +1,11 @@
 function expand_settings() {
   document.getElementById("settings").style.width = "250px";
+  //document.getElementById("settings_item_1").style.width = "250px";
 }
 function collapse_settings() {
   document.getElementById("settings").style.width = "0px";
+  //document.getElementById("settings_item_1").style.width = "0px";
+}
+function activate() {
+    collapse_settings();
 }
